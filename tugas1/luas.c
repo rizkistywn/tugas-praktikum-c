c690aed79ae7abf8d93816f0b6f905e7cb25f0a4
Rizki Setiyawan <141109540+rizkistywn@users.noreply.github.com>
2023-10-12 13:57:41 +0700

    Add files via upload

---
+++ tugas1/luas.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main()
+{
+    int panjang = 10;
+    int lebar = 7;
+
+    printf("Panjang %d cm.\n",panjang);
+    printf("Lebar %d cm.\n",lebar);
+
+    return 0;
+}
---
+++ tugas1/nim.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+int main() {
+
+    char nim[] = "2143017";
+    char nama[] = "Rizki setiyawan"; 
+    char jurusan[] = "Teknik Informatika";
+
+    printf("NIM    : %s\n", nim);
+    printf("NAMA   : %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
