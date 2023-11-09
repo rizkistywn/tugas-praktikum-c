169e9a10f34a246a158dd8ecdf4d8eb5fdd601a5
Rizki Setiyawan <141109540+rizkistywn@users.noreply.github.com>
2023-11-09 13:56:37 +0700

    Add files via upload

---
+++ UTS_C/UTS_C.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int tingkat_kekeruhan[] = {6, 8, 12, 18, 24, 30, 36, 42, 49};
+    int kekeruhan;
+    int kecepatan_motor, waktu_pencucian;
+
+    printf("Tombol Start ditekan.\n");
+
+    printf("Motor pembuka klep pengisian air aktif selama 2 menit.\n");
+    sleep(2 * 60);
+
+    printf("Masukkan nilai level kekeruhan air: ");
+    scanf("%d", &kekeruhan);
+
+    for (int i = 0; i < sizeof(tingkat_kekeruhan) / sizeof(tingkat_kekeruhan[0]); i++) {
+        if (kekeruhan <= tingkat_kekeruhan[i]) {
+            kecepatan_motor = (i + 1) * 1000;
+            waktu_pencucian = 60;
+            break;
+        }
+    }
+
+    printf("Motor listrik utama berputar dengan kecepatan %d rpm selama %d menit.\n", kecepatan_motor, waktu_pencucian);
+    printf("Motor pembuka klep pembuangan air kotor aktif selama 2 menit.\n");
+    sleep(2 * 60);
+
+    printf("Motor listrik utama berputar dengan kecepatan 2000 rpm untuk mengeringkan cucian selama 3 menit.\n");
+    sleep(3 * 60);
+
+    printf("Motor pembuka klep pengisian air aktif selama 2 menit.\n");
+    sleep(2 * 60);
+
+    printf("Motor listrik utama berputar dengan kecepatan %d rpm selama sisa waktu mencuci.\n", kecepatan_motor);
+    printf("Motor pembuka klep pembuangan air kotor aktif selama 2 menit.\n");
+    sleep(2 * 60);
+
+    printf("Motor listrik utama berputar dengan kecepatan 2000 rpm untuk mengeringkan cucian selama 3 menit.\n");
+    sleep(3 * 60);
+
+    printf("Proses mencuci selesai.\n");
+
+    return 0;
+}
